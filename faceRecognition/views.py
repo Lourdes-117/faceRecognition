324c72b96984fa3b773746ae80bdc60cb9aa59fd
Lourdes Dinesh <lourdes.dinesh@eka.care>
2024-08-24 15:46:16 +0530

    Add logs

--- faceRecognition/views.py
+++ faceRecognition/views.py
@@ -6,16 +6,30 @@ from io import BytesIO
 
 @csrf_exempt
 def face_recognition(request):
+    print("Request received")
+
+    print(request.FILES)
+
     original_img = request.FILES.get('original_pic')
+
+    print("Original image received")
     current_img = request.FILES.get('current_pic')
+
+    print("Current image received")
     
     if original_img is None or current_img is None:
+        print("Invalid input. Both 'original_pic' and 'current_pic' are required.")
         return HttpResponseBadRequest("Invalid input. Both 'original_pic' and 'current_pic' are required.")
-
+    
+    print("Processing images")
     try:
         # Create temporary files
         with tempfile.NamedTemporaryFile(delete=False, suffix=".jpg") as temp_original, \
              tempfile.NamedTemporaryFile(delete=False, suffix=".jpg") as temp_current:
+            print("Temporary files created")
+
+            print(original_img.size)
+            print(current_img.size)
             
             # Save images to temporary files
             temp_original.write(original_img.read())
@@ -30,6 +44,9 @@ def face_recognition(request):
                 temp_original.name,
                 temp_current.name
             )
+
+            print("Verification completed") 
+            print(request)
 
         return JsonResponse(result)
 
