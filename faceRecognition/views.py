e43db90770869d9858992b3fd4ed8f652a314d02
Lourdes Dinesh <lourdes.dinesh@eka.care>
2024-12-30 15:19:28 +0530

    Handle image as bytes

--- faceRecognition/views.py
+++ faceRecognition/views.py
@@ -8,42 +8,37 @@ from io import BytesIO
 def face_recognition(request):
     print("Request received")
 
-    print(request.FILES)
-
-    original_img = request.FILES.get('original_pic')
-
-    print("Original image received")
-    current_img = request.FILES.get('current_pic')
-
-    print("Current image received")
-    
-    if original_img is None or current_img is None:
-        print("Invalid input. Both 'original_pic' and 'current_pic' are required.")
+    try:
+        original_img = BytesIO(request.FILES['original_pic'].read())
+        current_img = BytesIO(request.FILES['current_pic'].read())
+    except KeyError:
         return HttpResponseBadRequest("Invalid input. Both 'original_pic' and 'current_pic' are required.")
-    
+    except Exception as e:
+        return HttpResponseBadRequest(f"Error reading image data: {str(e)}")
+
     print("Processing images")
     try:
         # Create temporary files
-        with tempfile.NamedTemporaryFile(delete=False, suffix=".jpg") as temp_original, \
-             tempfile.NamedTemporaryFile(delete=False, suffix=".jpg") as temp_current:
+        with tempfile.NamedTemporaryFile(delete=True, suffix=".jpg") as temp_original, \
+             tempfile.NamedTemporaryFile(delete=True, suffix=".jpg") as temp_current:
             print("Temporary files created")
 
             print(original_img.size)
             print(current_img.size)
             
             # Save images to temporary files
-            temp_original.write(original_img.read())
-            temp_current.write(current_img.read())
-            
-            # Make sure to close the files so DeepFace can read them
-            temp_original.close()
-            temp_current.close()
+            temp_original.write(original_img.getvalue())
+            temp_current.write(current_img.getvalue())
             
             # Perform the verification with DeepFace
             result = DeepFace.verify(
                 temp_original.name,
                 temp_current.name
             )
+
+            # Make sure to close the files so DeepFace can read them
+            temp_original.close()
+            temp_current.close()
 
             print("Verification completed") 
             print(request)
